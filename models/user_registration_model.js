20f8f56531462da9536e42a304137e1ddf207ee7
pasidhu naveen <pasidhu878@gmail.com>
2023-11-02 11:20:51 +0530

    Refactoring

--- models/user_registration_model.js
+++ models/user_registration_model.js
@@ -1,62 +1,26 @@
-// Import the 'mongoose' library for MongoDB operations.
 const mongoose = require('mongoose');
 
-/**
- * Define a Mongoose schema for the 'userRegistration' collection.
- */
 const userRegistrationSchema = new mongoose.Schema({
-    // Define a field 'name'
-    name: {
-        type: String,
-        required: true,
-    },
-    // Define a field 'email'
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    // Define a field 'mobile-number'
-    mobile: {
-        type: String,
-        required: true,
-    },
-    // Define a field 'birthday'
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true },
     birthday: Date,
-    // Define a field 'address'
     address: String,
-    // Define a field 'profession'
     profession: String,
-    // Define a field 'location'
     location: {
-        type: {
-            type: String,
-            default: 'Point',
-        },
+        type: { type: String, default: 'Point' },
         coordinates: {
             type: [Number],
             index: '2dsphere',
         },
     },
-    // Define a field 'password'
-    password: {
-        type: String,
-        required: true,
-    },
-    // Define a field 'fcm_token'
+    password: { type: String, required: true },
     fcm_token: String,
-    // Define a field 'createdOn'
-    createdOn: {
-        type: Date,
-        default: Date.now,
-    },
+    createdOn: { type: Date, default: Date.now },
 });
 
-// Create a geospatial index for the 'location' field.
 userRegistrationSchema.index({ location: '2dsphere' });
 
-// Create a Mongoose model for 'UserRegistration' using the schema.
 const UserRegistration = mongoose.model('UserRegistration', userRegistrationSchema);
 
-// Export the UserRegistration model.
 module.exports = UserRegistration;

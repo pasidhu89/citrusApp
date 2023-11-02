e60cfe0f6b16f9556ed2c199a4c34022bd4ae8c9
pasidhu naveen <pasidhu878@gmail.com>
2023-11-02 11:31:01 +0530

    Implement as a Class

--- models/upload_records.js
+++ models/upload_records.js
@@ -1,45 +1,49 @@
 import mongoose from 'mongoose';
 
-const uploadRecordSchema = new mongoose.Schema({
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-    },
-    symptom_type: {
-        type: String,
-        required: true,
-    },
-    category: String,
-    affected_radius: String,
-    location: {
-        type: {
-            type: String,
-            default: 'Point',
-        },
-        coordinates: {
-            type: [Number],
-            index: '2dsphere',
-        },
-    },
-    recent_treatments: String,
-    status: String,
-    image_urls: {
-        type: [String],
-        required: true,
-    },
-    output_urls: {
-        type: [String],
-        required: true,
-    },
-    output_labels: [String],
-    created_at: {
-        type: Date,
-        default: Date.now,
-    },
-});
+class UploadRecord {
+    constructor() {
+        const uploadRecordSchema = new mongoose.Schema({
+            user_id: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+            },
+            symptom_type: {
+                type: String,
+                required: true,
+            },
+            category: String,
+            affected_radius: String,
+            location: {
+                type: {
+                    type: String,
+                    default: 'Point',
+                },
+                coordinates: {
+                    type: [Number],
+                    index: '2dsphere',
+                },
+            },
+            recent_treatments: String,
+            status: String,
+            image_urls: {
+                type: [String],
+                required: true,
+            },
+            output_urls: {
+                type: [String],
+                required: true,
+            },
+            output_labels: [String],
+            created_at: {
+                type: Date,
+                default: Date.now,
+            },
+        });
 
-uploadRecordSchema.index({ location: '2dsphere' });
+        uploadRecordSchema.index({ location: '2dsphere' });
 
-const UploadRecord = mongoose.model('UploadRecord', uploadRecordSchema);
+        this.model = mongoose.model('UploadRecord', uploadRecordSchema);
+    }
+}
 
-export default UploadRecord;
+export default new UploadRecord().model;

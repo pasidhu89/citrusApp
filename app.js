4310856ba052ffdbe775cbb2a23eabfe16f7494d
pasidhu naveen <pasidhu878@gmail.com>
2023-11-02 11:29:14 +0530

    Encapsulate Database Connection Logic

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const mongoose = require('mongoose');
 const port = 3000;
 app.use(express.json());
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'));
 
 const loginRoute = require('./routes/authentication/login');
 const registerationRoute = require('./routes/authentication/registration');
 const uploadImages = require('./routes/upload/upload');
 
-mongoose.connect('mongodb://localhost:27017/cirtus_farm_db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-const db = mongoose.connection;
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/cirtus_farm_db', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
+};
 
-db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-});
-
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
+connectToDatabase();
 
 app.use('/login', loginRoute);
 app.use('/register', registerationRoute);

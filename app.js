875d967d6c2b5c73d34595e3f073c30bff6d2d41
pasidhu naveen <pasidhu878@gmail.com>
2023-11-02 11:34:47 +0530

    Use Async/Await for MongoDB Connection

--- app.js
+++ app.js
@@ -4,32 +4,28 @@ const mongoose = require('mongoose');
 const port = 3000;
 app.use(express.json());
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'));
 
 const loginRoute = require('./routes/authentication/login');
 const registerationRoute = require('./routes/authentication/registration');
 const uploadImages = require('./routes/upload/upload');
 
-const dbURL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/cirtus_farm_db';
+(async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/cirtus_farm_db', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
 
-mongoose.connect(dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-const db = mongoose.connection;
+    app.use('/login', loginRoute);
+    app.use('/register', registerationRoute);
+    app.use('/upload', uploadImages);
 
-db.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-});
-
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
-
-app.use('/login', loginRoute);
-app.use('/register', registerationRoute);
-app.use('/upload', uploadImages);
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+})();

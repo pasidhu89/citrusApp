fd140538f72dd1ebb1bc0d6eea2263349cf9e2ae
pasidhu naveen <pasidhu878@gmail.com>
2023-11-02 11:27:40 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = 3000;
+app.use(express.json());
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const loginRoute = require('./routes/authentication/login');
+const registerationRoute = require('./routes/authentication/registration');
+const uploadImages = require('./routes/upload/upload');
+
+mongoose.connect('mongodb://localhost:27017/cirtus_farm_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.use('/login', loginRoute);
+app.use('/register', registerationRoute);
+app.use('/upload', uploadImages);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});

bc36a62a26dfe196ba0adaf926de942d12ce39bc
pasidhu naveen <pasidhu878@gmail.com>
2023-11-02 11:29:44 +0530

    Change Response Messages

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,13 +4,12 @@ function verifyToken(req, res, next) {
     const token = req.headers.authorization;
 
     if (!token) {
-        return res.status(401).json({ message: 'Unauthorized: Token missing' });
+        return res.status(401).json({ message: 'Access Denied: Token missing' });
     }
 
     jwt.verify(token, 'your-secret-key', (err, decoded) => {
         if (err) {
-            console.error('JWT verification failed:', err);
-            return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+            return res.status(401).json({ message: 'Access Denied: Invalid token' });
         }
 
         req.userId = decoded.userId;

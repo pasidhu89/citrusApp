8b254173fbde3d8c105d443c31e1fe386ea8523b
pasidhu naveen <pasidhu878@gmail.com>
2023-11-02 11:13:56 +0530

    Separate Admin Initialization

--- middlewares/notification.js
+++ middlewares/notification.js
@@ -1,2 +1,19 @@
 
 
+const admin = require('../app');
+
+class FCM {
+    async sendMessage(title, body, registrationToken) {
+        const message = {
+            notification: {
+                title: title,
+                body: body,
+            },
+            token: registrationToken,
+        };
+
+        return admin.messaging().send(message);
+    }
+}
+
+module.exports = FCM;
